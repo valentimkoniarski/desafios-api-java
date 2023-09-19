c7c527a7e2aa848ce56915afb07b19259f0560e5
valentim.koniarski <valentim.koniarski@hotmail.com>
2023-09-18 22:54:30 -0300

    criado queries para setar o saldo do cliente

--- src/main/java/com/picpay/picpaydesafio/repositories/ClienteRepository.java
+++ src/main/java/com/picpay/picpaydesafio/repositories/ClienteRepository.java
@@ -2,10 +2,12 @@ package com.picpay.picpaydesafio.repositories;
 
 import com.picpay.picpaydesafio.entities.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.math.BigDecimal;
 
 @Repository
 @Transactional
@@ -13,4 +15,13 @@ public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
     @Query(value = "SELECT CASE WHEN EXISTS (SELECT * FROM clientes WHERE usuario_id = ?1) THEN TRUE ELSE FALSE END", nativeQuery = true)
     boolean findClienteByUsuarioId(Long usuarioId);
+
+    @Modifying
+    @Query(value = "UPDATE clientes SET saldo =+ saldo + ?1 WHERE usuario_id = ?2", nativeQuery = true)
+    void updateSaldoClienteRecebedor(BigDecimal saldo, Long usuarioId);
+
+    @Modifying
+    @Query(value = "UPDATE clientes SET saldo =+ saldo - ?1 WHERE usuario_id = ?2", nativeQuery = true)
+    void updateSaldoClienteTransferidor(BigDecimal saldo, Long usuarioId);
+
 }
