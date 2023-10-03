d6af12872095d85e6e777606c5cc8dd5b7c63a57
valentim.koniarski <valentim.koniarski@hotmail.com>
2023-10-02 23:25:51 -0300

    removido HttpServletRequest do controller e deixado na service

--- src/main/java/com/picpay/picpaydesafio/controllers/TransferenciaController.java
+++ src/main/java/com/picpay/picpaydesafio/controllers/TransferenciaController.java
@@ -3,7 +3,7 @@ package com.picpay.picpaydesafio.controllers;
 import com.picpay.picpaydesafio.dtos.TransferenciaDto;
 import com.picpay.picpaydesafio.exceptions.TransferenciaValidacoesException;
 import com.picpay.picpaydesafio.services.TransferenciaService;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,22 +11,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/transferencias")
-@AllArgsConstructor
 public class TransferenciaController {
 
     private final TransferenciaService transferenciaService;
 
+    private final HttpServletRequest request;
+
+    @Autowired
+    public TransferenciaController(TransferenciaService transferenciaService, HttpServletRequest request) {
+        this.transferenciaService = transferenciaService;
+        this.request = request;
+    }
+
     @PostMapping
-    public ResponseEntity transferenciaEntreClientes(HttpServletRequest request,
-                                                     @RequestBody TransferenciaDto transferenciaDto) {
+    public ResponseEntity transferenciaEntreClientes(@Valid @RequestBody TransferenciaDto transferenciaDto) {
         try {
             transferenciaService.transferencia(request, transferenciaDto);
             return ResponseEntity.ok().build();
-        } catch (TransferenciaValidacoesException.LojistaNaoPodeTransferirException
-                 | TransferenciaValidacoesException.SaldoNaoPodeSerNegativoException e) {
+        } catch (TransferenciaValidacoesException.LojistaNaoPodeTransferirException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (TransferenciaValidacoesException.SaldoNaoPodeSerNegativoException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
