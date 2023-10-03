d0dfd6f8294d67ff586ba5c98c4e26c8924986f6
valentim.koniarski <valentim.koniarski@hotmail.com>
2023-10-02 23:25:24 -0300

    removido HttpServletRequest do controller e deixado na service

--- src/main/java/com/picpay/picpaydesafio/controllers/ClienteController.java
+++ src/main/java/com/picpay/picpaydesafio/controllers/ClienteController.java
@@ -1,27 +1,32 @@
 package com.picpay.picpaydesafio.controllers;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.picpay.picpaydesafio.exceptions.ClienteValidacoesException;
 import com.picpay.picpaydesafio.services.ClienteService;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping(value = "/clientes")
-@AllArgsConstructor
 public class ClienteController {
 
     private final ClienteService clienteService;
 
+    @Autowired
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
     @PostMapping("/cadastrar")
-    public ResponseEntity cadastrarCliente(HttpServletRequest request) {
+    public ResponseEntity<String> cadastrarCliente() {
         try {
-            clienteService.salvar(request);
+            clienteService.salvar();
             return ResponseEntity.ok().build();
         } catch (ClienteValidacoesException.ClienteExistenteException |
                  ClienteValidacoesException.SaldoInicialInvalidoException e) {
